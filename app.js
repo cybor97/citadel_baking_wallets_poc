c6d8865bd428c4ebfe4c110056fcc8df12b7c959
cybor97 <muhametshin.1997@gmail.com>
2019-07-07 22:02:37 +0300

    Added CI and apidoc

--- app.js
+++ app.js
@@ -4,13 +4,32 @@ const fs = require('fs');
 const path = require('path');
 const expect = require('./expect');
 const app = express();
-const EXPECT_IMPORT_KEY_TEMPLATE = fs.readFileSync(path.join(__dirname, 'importKeyTemplate.sh'));
-const EXPECT_CREATE_KEY_TEMPLATE = fs.readFileSync(path.join(__dirname, 'createKeyTemplate.sh'));
-const EXPECT_START_BAKING_TEMPLATE = fs.readFileSync(path.join(__dirname, 'startBakingTemplate.sh'));
+const EXPECT_IMPORT_KEY_TEMPLATE = fs.readFileSync(path.join(__dirname, 'templates/importKeyTemplate.sh'));
+const EXPECT_CREATE_KEY_TEMPLATE = fs.readFileSync(path.join(__dirname, 'templates/createKeyTemplate.sh'));
+const EXPECT_START_BAKING_TEMPLATE = fs.readFileSync(path.join(__dirname, 'templates/startBakingTemplate.sh'));
 
 app
 .use(express.json())
 .use(express.urlencoded({extended: true}))
+.use('/doc', express.static(path.join(__dirname, 'doc')))
+
+/**
+ * @api {get} /importKey Import key data
+ * @apiName importKey
+ * @apiGroup keys
+ * @apiDescription Import key data for tezos-client and tezos-baker
+ * 
+ * @apiParam {String} accName               Unique name of account    
+ * @apiParam {String} accEmail              Account email
+ * @apiParam {Array}  words                 14 secret words
+ * @apiParam {String} paperWalletPassword   Paper wallet password
+ * @apiParam {String} encryptionPassword    Encryption password
+ *
+ * @apiSuccess {String} output Data from stdout(tezos-client response)
+ * @apiSuccess {String} errors Data from stderr(tezos-client error or warning/notification)
+ * @apiSuccess {String} code   Process exit code
+ * @apiSuccess {String} signal Process exited by signal
+ */
 .post('/importKey', (req, res) => {
     let procInstanceCheckBalance = proc.spawn('tezos-client', ['get', 'balance', 'for', req.body.accName]);
 
@@ -40,16 +59,47 @@ app
         }
     })
 })
-.post('/startBaking', (req, res) => {
-    let startBakingKeyScript = EXPECT_START_BAKING_TEMPLATE.toString()
-        .replace(/ACC_NAME/, req.body.accName)
-        .replace(/ENCRYPTION_PASSWORD/g, req.body.encryptionPassword);
-    expect.runInExpect(startBakingKeyScript, result => res.status(200).send(result));
-})
+
+/**
+ * @api {get} /createKey Create key pair
+ * @apiName createKey
+ * @apiGroup keys
+ * @apiDescription Create key pair for tezos-client and tezos-baker
+ * 
+ * @apiParam {String} accName               Unique name of account    
+ * @apiParam {String} encryptionPassword    Encryption password
+ *
+ * @apiSuccess {String} output Data from stdout(tezos-client response)
+ * @apiSuccess {String} errors Data from stderr(tezos-client error or warning/notification)
+ * @apiSuccess {String} code   Process exit code
+ * @apiSuccess {String} signal Process exited by signal
+ */
 .post('/createKey', (req, res) => {
     let createKeyScript = EXPECT_CREATE_KEY_TEMPLATE.toString()
         .replace(/ACC_NAME/, req.body.accName)
         .replace(/ENCRYPTION_PASSWORD/g, req.body.encryptionPassword);
     expect.runInExpect(createKeyScript, result => res.status(200).send(result));
 })
-.listen(8081);
+
+/**
+ * @api {get} /startBaking Start baking
+ * @apiName startBaking
+ * @apiGroup keys
+ * @apiDescription Start baking with tezos-baker
+ * 
+ * @apiParam {String} accName               Unique name of account    
+ * @apiParam {String} encryptionPassword    Encryption password
+ *
+ * @apiSuccess {String} output Data from stdout(tezos-baker response)
+ * @apiSuccess {String} errors Data from stderr(tezos-baker error or warning/notification)
+ * @apiSuccess {String} code   Process exit code
+ * @apiSuccess {String} signal Process exited by signal
+ */
+.post('/startBaking', (req, res) => {
+    let startBakingKeyScript = EXPECT_START_BAKING_TEMPLATE.toString()
+        .replace(/ACC_NAME/, req.body.accName)
+        .replace(/ENCRYPTION_PASSWORD/g, req.body.encryptionPassword);
+    expect.runInExpect(startBakingKeyScript, result => res.status(200).send(result));
+})
+
+.listen(8080);
